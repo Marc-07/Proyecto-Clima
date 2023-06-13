d5c533e80332bfac80f715c2b76bd08e4ab22411
Marc-07 <maanraca01@gmail.com>
2023-06-13 14:53:13 -0500

    Se crea alerta para mostrar error

--- js/app.js
+++ js/app.js
@@ -12,8 +12,40 @@ function buscarClima (e){
 
     //Validar
 
-    const ciudad = document.querySelector('#ciudad');
-    const pais = document.querySelector ('#pais');
+    const ciudad = document.querySelector('#ciudad').value;
+    const pais = document.querySelector ('#pais').value;
 
-    //Consultar API9
+    if( ciudad ===''     || pais === ''){
+
+        //Hubo un error
+
+        mostrarError('Ambos campos son aobligatorios');
+
+        return;
+        
+    }
+
+    function mostrarError(mensaje){
+        const alerta = document.querySelector('.bg-red-200');
+
+        if ( !alerta){
+
+            // Crear una alerta
+            const alerta = document.createElement('div');
+
+            alerta.classList.add('bg-red-200', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center');
+
+            alerta.innerHTML = `
+            <strong class ='font-bold'>Error!</strong>
+            <span class = 'block'>${mensaje}</span>
+        `;
+
+        container.appendChild(alerta);
+
+        setTimeout(() => {
+            alerta.remove(mensaje);
+        }, 3000);
+
+        }  
+    }
 }
